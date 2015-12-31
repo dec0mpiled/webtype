ea6cd5314d503d4530472d72f2cfe5fb42ac1a4e
Oliver Pope <opope@me.com>
2015-12-31 16:45:39 -0500

    radical changes

--- app.js
+++ app.js
@@ -1,110 +1,115 @@
 // dotenv (environment variables) (include .env)
-require('dotenv').load();
+require( 'dotenv' ).load();
 
 // dependencies
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var moment = require('moment');
-var LocalStrategy = require('passport-local').Strategy;
+var express = require( 'express' );
+var path = require( 'path' );
+var favicon = require( 'serve-favicon' );
+var logger = require( 'morgan' );
+var cookieParser = require( 'cookie-parser' );
+var bodyParser = require( 'body-parser' );
+var mongoose = require( 'mongoose' );
+var passport = require( 'passport' );
+var moment = require( 'moment' );
+var LocalStrategy = require( 'passport-local' ).Strategy;
 
-var routes = require('./routes/index');
-var auth = require('./routes/auth');
+var routes = require( './routes/index' );
+var auth = require( './routes/auth' );
 
-var c = require('./routes/c');
-var e = require('./routes/e');
-var d = require('./routes/d');
+var c = require( './routes/c' );
+var e = require( './routes/e' );
+var d = require( './routes/d' );
 
 var app = express();
 
-var hbs = require('hbs');
+var hbs = require( 'hbs' );
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'view engine', 'hbs' );
 
-hbs.registerHelper('equal', function(lvalue, rvalue, options) {
-    if (arguments.length < 3)
-        throw new Error("Handlebars Helper equal needs 2 parameters");
-    if( lvalue!=rvalue ) {
-        return options.inverse(this);
+hbs.registerHelper( 'equal', function( lvalue, rvalue, options ) {
+    if ( arguments.length < 3 )
+        throw new Error( "Handlebars Helper equal needs 2 parameters" );
+    if ( lvalue != rvalue ) {
+        return options.inverse( this );
     } else {
-        return options.fn(this);
+        return options.fn( this );
     }
-});
+} );
 
-hbs.registerHelper('date', function(value) {
-   return moment(value).format('MMM DD, YYYY');
-}); 
+hbs.registerHelper( 'date', function( value ) {
+    return moment( value ).format( 'MMM DD, YYYY' );
+} );
+hbs.registerHelper( 'dateEdited', function( value ) {
+    return moment( value ).fromNow();
+} );
 
-app.set("jsonp callback", true);
+app.set( "jsonp callback", true );
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('express-session')({
+app.use( logger( 'dev' ) );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( {
+    extended: false
+} ) );
+app.use( cookieParser() );
+app.use( require( 'express-session' )( {
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+} ) );
+app.use( passport.initialize() );
+app.use( passport.session() );
+app.use( express.static( path.join( __dirname, 'public' ) ) );
 
 
-app.use('/', routes);
-app.use('/auth', auth);
-app.use('/c', c);
-app.use('/e', e);
-app.use('/d', d);
+app.use( '/', routes );
+app.use( '/auth', auth );
+app.use( '/c', c );
+app.use( '/e', e );
+app.use( '/d', d );
 
 // passport config
-var Account = require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
+var Account = require( './models/account' );
+passport.use( new LocalStrategy( Account.authenticate() ) );
+passport.serializeUser( Account.serializeUser() );
+passport.deserializeUser( Account.deserializeUser() );
 
 // mongoose
-mongoose.connect(process.env.MONGOURI);
+mongoose.connect( process.env.MONGOURI );
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+app.use( function( req, res, next ) {
+    var err = new Error( 'Not Found' );
     err.status = 404;
-    next(err);
-});
+    next( err );
+} );
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
+if ( app.get( 'env' ) === 'development' ) {
+    app.use( function( err, req, res, next ) {
+        res.status( err.status || 500 );
+        res.render( 'error', {
             message: err.message,
             error: err
-        });
-    });
+        } );
+    } );
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+app.use( function( err, req, res, next ) {
+    res.status( err.status || 500 );
+    res.render( 'error', {
         message: err.message,
         error: {}
-    });
-});
+    } );
+} );
 
 
 module.exports = app;
