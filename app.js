5cc0721b391b2f61196498d35b685df04a1b4b3c
Oliver Pope <opope@me.com>
2016-05-05 14:19:26 -0400

    keymetrics

--- app.js
+++ app.js
@@ -33,36 +33,42 @@ var d              = require('./routes/d');
 if (app.get('env') === 'development') {
   require('dotenv').load();
 } else {
+  var MACHINE_NAME = 'hk1';
+  var PRIVATE_KEY  = process.env.PRIVATE_KEY;   // Keymetrics Private key
+  var PUBLIC_KEY   = process.env.PUBLIC_KEY;   // Keymetrics Public  key
+  
   var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
-  var maxMemory = process.env.WEB_MEMORY || 512;    // " " "
+  var maxMemory = process.env.WEB_MEMORY      || 512;// " " "
   
   pm2.connect(function() {
     pm2.start({
-      script    : './bin/www',
-      name      : 'owebboy',     // ----> THESE ATTRIBUTES ARE OPTIONAL:
+      script    : 'app.js',
+      name      : 'webtype',     // ----> THESE ATTRIBUTES ARE OPTIONAL:
       exec_mode : 'cluster',            // ----> https://github.com/Unitech/PM2/blob/master/ADVANCED_README.md#schema
       instances : instances,
       max_memory_restart : maxMemory + 'M',   // Auto restart if process taking more than XXmo
       env: {                            // If needed declare some environment variables
         "NODE_ENV": "production"
       },
-    }, function(err) {
-      if (err) return console.error('Error while launching applications', err.stack || err);
-      console.log('PM2 and application has been succesfully started');
+      post_update: ["npm install"]       // Commands to execute once we do a pull from Keymetrics
+    }, function() {
+      pm2.interact(PRIVATE_KEY, PUBLIC_KEY, MACHINE_NAME, function() {
   
-      // Display logs in standard output 
-      pm2.launchBus(function(err, bus) {
-        console.log('[PM2] Log streaming started');
+       // Display logs in standard output 
+       pm2.launchBus(function(err, bus) {
+         console.log('[PM2] Log streaming started');
   
-        bus.on('log:out', function(packet) {
-         console.log('[App:%s] %s', packet.process.name, packet.data);
+         bus.on('log:out', function(packet) {
+          console.log('[App:%s] %s', packet.process.name, packet.data);
+         });
+  
+         bus.on('log:err', function(packet) {
+           console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+         });
         });
   
-        bus.on('log:err', function(packet) {
-          console.error('[App:%s][Err] %s', packet.process.name, packet.data);
-        });
+  
       });
-  
     });
   });
 }
